ee345cb1b96f5a2828abfd0f3b7e6cb8082656b9
James Adams <james.adams@stfc.ac.uk>
2018-11-16 16:03:13 +0000

    Ensure Problems are properties of Lines in check_line_patterns and check_line_paths

--- panc/src/main/scripts/panlint/panlint.py
+++ panc/src/main/scripts/panlint/panlint.py
@@ -358,7 +358,7 @@ def check_line_patterns(line, string_ranges):
                 start, end = match.span('error')
                 debug_range(start, end, 'LineRE Match', True)
                 if not inside_string(start, end, string_ranges):
-                    problems.append(Problem(line, (start, end), message))
+                    problems.append(Problem(start, end, message))
     return problems
 
 
@@ -382,7 +382,7 @@ def check_line_paths(line):
                     start += path_start + 1
                     end += path_start + 1
                     debug_range(start, end, 'PathRE Match', True)
-                    problems.append(Problem(line, (start, end), message))
+                    problems.append(Problem(start, end, message))
 
     return problems
 
