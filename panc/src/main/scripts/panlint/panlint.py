2c091e25ec7537fc98a0d9b1088deafa6f0dfc3e
James Adams <james.adams@stfc.ac.uk>
2018-11-16 16:01:52 +0000

    Create Message object for CU001

--- panc/src/main/scripts/panlint/panlint.py
+++ panc/src/main/scripts/panlint/panlint.py
@@ -385,8 +385,9 @@ def check_line_component_use(line, components_included):
         if match.group('name') not in components_included:
             start, end = match.span('name')
             debug_range(start, end, 'ComponentUse', True)
-            message = 'Component %s in use, but component config has not been included' % match.group('name')
-            problems.append(Problem(line, (start, end), message))
+            message_text = 'Component %s in use, but component config has not been included' % match.group('name')
+            message = Message('CU001', SEV_WARNING, message_text)
+            problems.append(Problem(start, end, message))
     return problems
 
 
